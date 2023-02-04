1de1c64bf39a3ad6143d1891848f4383371271c2
migueldiasbrito <migueldiasbrito@gmail.com>
2023-02-04 23:18:27 +0000

    Add tree

---
+++ RootsVsBugsCore/Assets/Scripts/ProtoGameView/ProtoTreeView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoTreeView : MonoBehaviour
+{
+    public RectTransform tree;
+    public float timeToWin;
+
+    private float currentTime = 0;
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        Vector2 sizeDelta = tree.sizeDelta;
+        sizeDelta.y = 100 + 980 * currentTime / timeToWin;
+        tree.sizeDelta = sizeDelta;
+
+        //if (currentTime >= timeToWin) Debug.Log("Ganhou");
+    }
+}
