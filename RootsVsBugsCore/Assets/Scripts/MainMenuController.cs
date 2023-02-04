4562c27986c48e16d0161c31f78ea264811dc368
migueldiasbrito <migueldiasbrito@gmail.com>
2023-02-04 11:02:49 +0000

    Rename SS GameScene

--- RootsVsBugsCore/Assets/Scripts/MainMenuController.cs
+++ RootsVsBugsCore/Assets/Scripts/MainMenuController.cs
@@ -5,7 +5,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void GoToGameScene()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("SinglePlayerGameScene");
     }
 
     public void ExitGame()
