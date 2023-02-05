474df33611768d8e44b564091a8c85a87b9914cf
migueldiasbrito <migueldiasbrito@gmail.com>
2023-02-05 15:37:21 +0000

    Stop music in end screen

--- RootsVsBugsCore/Assets/Scripts/MainMenuController.cs
+++ RootsVsBugsCore/Assets/Scripts/MainMenuController.cs
@@ -30,17 +30,21 @@ public class MainMenuController : MonoBehaviour
             case 0:
                 MainMenu.SetActive(true);
                 EndScreen.SetActive(false);
+                if (!EverPlayingMusic.instance.audioSource.isPlaying)
+                    EverPlayingMusic.instance.audioSource.Play();
                 break;
             case 1:
                 endScreenText.text = "DEFEAT";
                 MainMenu.SetActive(false);
                 EndScreen.SetActive(true);
+                EverPlayingMusic.instance.audioSource.Stop();
                 multiPurposeAudioSource.PlayOneShot(defeat);
                 break;
             case 2:
                 endScreenText.text = "VICTORY";
                 MainMenu.SetActive(false);
                 EndScreen.SetActive(true);
+                EverPlayingMusic.instance.audioSource.Stop();
                 multiPurposeAudioSource.PlayOneShot(victory);
                 break;
         }
@@ -49,6 +53,8 @@ public class MainMenuController : MonoBehaviour
     public void GoToGameScene()
     {
         MainMenuState = 0;
+        if (!EverPlayingMusic.instance.audioSource.isPlaying)
+            EverPlayingMusic.instance.audioSource.Play();
         SceneManager.LoadScene("SinglePlayerGameScene");
     }
 
