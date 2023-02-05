a463dfad0068c3786d39b502d12436ea45c65541
Moisés <liricractos@gmail.com>
2023-02-05 12:32:03 +0000

    push before the stomr

--- RootsVsBugsCore/Assets/Scripts/Multyplayer/PlayerGestor.cs
+++ RootsVsBugsCore/Assets/Scripts/Multyplayer/PlayerGestor.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerGestor : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public int ? _myID;
+    public Lane myLane;
+    public Player myPlayer;
+
+    public List<Plant> myPlants;
+
+    private void Start()
     {
-        
+        myPlayer.resourcesUpdated.AddListener((Resources resources) =>
+        {
+            print("ACONTECE");
+            string tradtada = "R" + resources.Minerals + "/" + resources.Water + "/" + resources.Seeds;
+            SendToPhone(_myID.ToString(), tradtada);
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LerETratarMsg(string msg)
     {
-        
+
+   
+        string idM = msg.Split(",")[0];
+        string mensagemRawRaw = msg.Split(",")[1];
+
+        if (_myID.ToString() == idM)
+        {
+            //Plantar
+            if (mensagemRawRaw.Substring(0, 1) == "P")
+            {
+                string mensagem = mensagemRawRaw.Split("P;")[1];
+
+
+                print("A Mensagem  " + mensagem);
+
+                string laneM = mensagem.Split(";")[1];
+                string plantaM = mensagem.Split(";")[0];
+                string suns = mensagem.Split(';')[2];
+
+                print("foi eu " + idM + "      L->" + laneM + "     Planta-> " + plantaM);
+
+
+                myPlayer.Resources.Minerals = int.Parse(suns);
+                print(myPlayer.Resources.Minerals);
+                myLane.AddPlantToSlot(myPlayer, myPlants[int.Parse(plantaM)], int.Parse(laneM));
+
+            }
+            //Enviar
+            if (mensagemRawRaw.Substring(0, 1) == "E")
+            {
+                string mensagemh = mensagemRawRaw.Split("E;")[1];
+                string IDAmigo = mensagemh.Split(";")[0];
+                string RecursoEnv = mensagemh.Split(';')[1];
+
+                print(" ID " + IDAmigo + "Recurso" + RecursoEnv);
+            }
+
+            if(mensagemRawRaw.Substring(0, 1) == "L")
+            {
+                
+                Destroy(myLane.gameObject);
+            }
+
+        }
+
+    }
+
+    public void SendToPhone(string id, string msg)
+    {
+        GestorGeral.SendResourceUpdateMessage(id, msg);
     }
 }
